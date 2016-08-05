c6caa5e53c5872f081a5105a4da34446a2f2f6de
reinoptland <reinoptland@gmail.com>
2016-08-05 11:06:44 +0200

    configured activerecord

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Pokemonshow
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    console do
+   ActiveRecord::Base.connection
   end
 end
